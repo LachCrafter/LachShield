e45201d9d8b217579db7433cfe62542a7d538d20
LachCrafter <100131971+LachCrafter@users.noreply.github.com>
2023-10-14 11:57:46 +0200

    Update IPCheckListener.java

--- src/main/java/de/lachcrafter/lachshield/IPCheckListener.java
+++ src/main/java/de/lachcrafter/lachshield/IPCheckListener.java
@@ -16,7 +16,6 @@ public class IPCheckListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        // Überprüfen, ob der Spieler die lachshield.admin-Berechtigung hat
         if (!player.hasPermission("lachshield.admin")) {
             int ipLimit = plugin.getConfigManager().getMaxAccountsPerIP();
         }
