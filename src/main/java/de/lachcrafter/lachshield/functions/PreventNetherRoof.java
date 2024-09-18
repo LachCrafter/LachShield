1cc47e2ce05981af10fc7803cb8a7c1f08f7cd14
LachCrafter <lachcrafteryt@gmail.com>
2024-09-18 20:09:23 +0200

    improve PreventnethherRoof logix

--- src/main/java/de/lachcrafter/lachshield/functions/PreventNetherRoof.java
+++ src/main/java/de/lachcrafter/lachshield/functions/PreventNetherRoof.java
@@ -1,12 +1,14 @@
 package de.lachcrafter.lachshield.functions;
 
-import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.Material;
 
 public class PreventNetherRoof implements Listener {
 
@@ -25,14 +27,25 @@ public class PreventNetherRoof implements Listener {
             return;
         }
 
+        if (player.hasPermission("lachshield.admin")) {
+            return;
+        }
+
         if (world.getEnvironment() == World.Environment.NETHER && player.getLocation().getY() >= 128) {
             event.setCancelled(true);
 
             String warningMessage = config.getString("prevent_nether_roof.warning_message", "<red>You cannot enter the Nether roof!");
+            player.sendMessage(MiniMessage.miniMessage().deserialize(warningMessage));
 
-            player.sendMessage(Component.text(warningMessage));
+            Location safeLocation = player.getLocation();
+            safeLocation.setY(127);
 
-            player.teleport(player.getLocation().set(player.getLocation().getX(), 127, player.getLocation().getZ()));
+            if (world.getBlockAt(safeLocation).getType() == Material.AIR && world.getBlockAt(safeLocation.add(0, 1, 0)).getType() == Material.AIR) {
+                player.teleport(safeLocation);
+            } else {
+                safeLocation.setY(126);
+                player.teleport(safeLocation);
+            }
         }
     }
 }
