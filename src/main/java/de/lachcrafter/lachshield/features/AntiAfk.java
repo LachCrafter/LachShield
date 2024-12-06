8a2f133db570ff012654e7f596f25c5ee6ad901b
LachCrafter <lachcrafteryt@gmail.com>
2024-12-07 00:23:51 +0100

    Remove player from playerActivity when the player quits

--- src/main/java/de/lachcrafter/lachshield/features/AntiAfk.java
+++ src/main/java/de/lachcrafter/lachshield/features/AntiAfk.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -41,6 +42,11 @@ public class AntiAfk implements Listener {
             return;
         }
         playerActivity.put(player.getUniqueId(), System.currentTimeMillis());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        playerActivity.remove(event.getPlayer().getUniqueId());
     }
 
     private void startAfkCheck() {
