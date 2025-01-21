ea46ac445e9ad52cdc57862c2dad638c116b3e67
LachCrafter <lachcrafteryt@gmail.com>
2025-01-21 08:05:16 +0100

    fix: use ipLimitKickMessage instead of afkKickMessage

--- src/main/java/de/lachcrafter/lachshield/features/IPAccountManager.java
+++ src/main/java/de/lachcrafter/lachshield/features/IPAccountManager.java
@@ -40,7 +40,7 @@ public class IPAccountManager implements Feature {
         int accountCount = ipAccountCount.getOrDefault(ip, 0);
 
         if (accountCount >= maxAccountsPerIP) {
-            Component kickComponent = configManager.getAfkKickMessage();
+            Component kickComponent = configManager.getIpLimitKickMessage();
             player.kick(kickComponent);
             return;
         }
