70e8377d17b4825f9cf16606494c765d5c998c67
lachcrafter <lachcrafteryt@gmail.com>
2025-07-05 23:54:32 +0200

    feat: add FeatureNew class

---
+++ src/main/java/de/lachcrafter/lachshield/features/TestFeature.java
@@ -0,0 +1,25 @@
+package de.lachcrafter.lachshield.features;
+
+import de.lachcrafter.lachshield.lib.NewFeature;
+
+public class TestFeature extends NewFeature {
+
+    public TestFeature() {
+        super("TestFeatureNew");
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onReload() {
+
+    }
+}
