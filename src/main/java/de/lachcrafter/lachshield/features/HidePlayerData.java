c6ac79e9042e9925121c5ef43262f1ad43133c23
lachcrafter <lachcrafteryt@gmail.com>
2026-03-27 16:45:37 +0100

    feat: rewrite HidePlayerData

--- src/main/java/de/lachcrafter/lachshield/features/HidePlayerData.java
+++ src/main/java/de/lachcrafter/lachshield/features/HidePlayerData.java
@@ -1,87 +1,105 @@
 package de.lachcrafter.lachshield.features;
 
 import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.EventManager;
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
 import com.github.retrooper.packetevents.protocol.item.enchantment.Enchantment;
 import com.github.retrooper.packetevents.protocol.item.enchantment.type.EnchantmentTypes;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.wrapper.play.server.*;
-import org.bukkit.configuration.file.FileConfiguration;
+import com.github.retrooper.packetevents.protocol.player.GameMode;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
+import de.lachcrafter.lachshield.LachShield;
+import org.jspecify.annotations.NonNull;
 
 import java.util.ArrayList;
-import java.util.Collections;
-
-import static de.lachcrafter.lachshield.LachShield.configManager;
+import java.util.List;
 
 public class HidePlayerData extends Feature {
-    PacketListener packetListener = new PacketListener();
+    private final EventManager eventManager = PacketEvents.getAPI().getEventManager();
+    private final PlayerDataListener playerDataListener;
+    protected static boolean hideHealth, hideEnchantments, hideDurability, hideStackSize, hideGameMode;
 
     public HidePlayerData() {
         super("HidePlayerData", true);
+        playerDataListener = new PlayerDataListener();
     }
 
     @Override
     public void onEnable() {
-        PacketEvents.getAPI().getEventManager().registerListener(packetListener);
+        eventManager.registerListener(playerDataListener);
     }
 
     @Override
     public void onDisable() {
-        PacketEvents.getAPI().getEventManager().unregisterListener(packetListener);
+        eventManager.unregisterListener(playerDataListener);
     }
 
     @Override
     public void onReload() {
-        packetListener.reload();
+        var config = LachShield.configManager.getConfig();
+
+        hideHealth = config.getBoolean(getName() + ".data.health");
+        hideEnchantments = config.getBoolean(getName() + ".data.enchantments");
+        hideDurability = config.getBoolean(getName() + ".data.durability");
+        hideStackSize = config.getBoolean(getName() + ".data.stackSize");
+        hideGameMode = config.getBoolean(getName() + ".data.gameMode");
     }
 }
 
-class PacketListener extends PacketListenerAbstract {
-
-    public boolean stackSize;
-    public boolean durability;
-    public boolean health;
-    public boolean enchantments;
+class PlayerDataListener extends PacketListenerAbstract {
 
     @Override
-    public void onPacketSend(PacketSendEvent event) {
-        if (event.getPacketType() == PacketType.Play.Server.ENTITY_EQUIPMENT) {
-            WrapperPlayServerEntityEquipment ee = new WrapperPlayServerEntityEquipment(event);
-
-            ee.getEquipment().forEach(eq -> {
-                if (stackSize) {
-                    eq.getItem().setAmount(1);
-                }
-                if (durability) {
-                    eq.getItem().setDamageValue(0);
-                }
-                if (enchantments && eq.getItem().isEnchanted()) {
-                    eq.getItem().setEnchantments(new ArrayList<>(Collections.singletonList(
-                            new Enchantment.Builder()
-                                    .type(EnchantmentTypes.ALL_DAMAGE_PROTECTION)
-                                    .level(1)
-                                    .build()
-                    )));
-                }
-            });
-        }
+    public void onPacketSend(@NonNull PacketSendEvent event) {
 
         if (event.getPacketType() == PacketType.Play.Server.ENTITY_METADATA) {
-            WrapperPlayServerEntityMetadata em = new WrapperPlayServerEntityMetadata(event);
+            WrapperPlayServerEntityMetadata packet = new WrapperPlayServerEntityMetadata(event);
 
-            if (health) {
-                em.getEntityMetadata().removeIf(data -> data.getIndex() == 9);
+            if (HidePlayerData.hideDurability) {
+                var playerItemStack = packet.readItemStack();
+
+                playerItemStack.setDamageValue(1);
+                packet.writeItemStack(playerItemStack);
             }
-        }
-    }
 
-    public void reload() {
-        FileConfiguration config = configManager.getConfig();
-        this.stackSize = config.getBoolean("HidePlayerData.data.stackSize", true);
-        this.durability = config.getBoolean("HidePlayerData.data.durability", true);
-        this.health = config.getBoolean("HidePlayerData.data.health", true);
-        this.enchantments = config.getBoolean("HidePlayerData.enchantments", true);
+            if (HidePlayerData.hideEnchantments) {
+                var playerItemStack = packet.readItemStack();
+
+                if (playerItemStack.isEnchanted()) {
+                    playerItemStack.setEnchantments(List.of(Enchantment.builder()
+                            .type(EnchantmentTypes.ALL_DAMAGE_PROTECTION)
+                            .level(1)
+                            .build()));
+
+                    packet.writeItemStack(playerItemStack);
+                }
+            }
+
+            if (HidePlayerData.hideStackSize) {
+                var playerItemStack = packet.readItemStack();
+
+                playerItemStack.setAmount(1);
+                packet.writeItemStack(playerItemStack);
+            }
+
+            if (HidePlayerData.hideGameMode) {
+                packet.writeGameMode(GameMode.ADVENTURE);
+            }
+
+            if (HidePlayerData.hideHealth) {
+                List<EntityData<?>> entityDataList = new ArrayList<>();
+
+                for (EntityData<?> data : packet.getEntityMetadata()) {
+
+                    if (data.getIndex() == 9) break;
+                    entityDataList.add(data);
+                }
+                packet.setEntityMetadata(entityDataList);
+            }
+
+            event.markForReEncode(true);
+        }
     }
 
 }
